d8e07d973f5b3e49900fbdd1dfe0082d4297e657
marcondesoliveira <marcondescurso@outlook.com>
2021-02-28 00:46:24 -0300

    Testes automatizados

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,30 +3,30 @@ import { getCustomRepository } from 'typeorm'
 import { UsersRepository } from '../repositories/UsersRepository'
 
 class UserController {
+  async create(request: Request, response: Response) {
+    const { name, email } = request.body
 
-    async create(request: Request, response: Response) {
-        const {name, email} = request.body
-        
-        const usersRepository = getCustomRepository(UsersRepository)
+    const usersRepository = getCustomRepository(UsersRepository)
 
-        const userAlreadyExists = await usersRepository.findOne({
-            email
-        })
+    const userAlreadyExists = await usersRepository.findOne({
+      email,
+    })
 
-        if(userAlreadyExists) {
-            return response.status(400).json({
-                error: "User already exists!"
-            })
-        }
-
-        const user = usersRepository.create({
-            name, email
-        })
-
-        await usersRepository.save(user)
-        
-        return response.json(user)
+    if (userAlreadyExists) {
+      return response.status(400).json({
+        error: 'User already exists!',
+      })
     }
+
+    const user = usersRepository.create({
+      name,
+      email,
+    })
+
+    await usersRepository.save(user)
+
+    return response.status(201).json(user)
+  }
 }
 
 export { UserController }
